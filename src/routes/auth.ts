bebb21312674ce6b00684590a810a9d8510a3945
Natalia Henao <nataliahenaor24@gmail.com>
2025-10-17 16:40:05 -0500

    fix dele comments

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,8 +2,7 @@ import { Router } from "express";
 import { register, login } from "../controllers/auth.js";
 
 const router = Router();
-// localhost:3000/auth/login
-// localhost:3000/auth/register
+
 router.post("/login", login);
 router.post("/register", register);
 
